d2c1bdc97582d0055c507e758c5395702f6c8c2f
Charles Bousseau <16641587+cbouss@users.noreply.github.com>
2024-10-10 09:02:40 -0400

    Update recipe/bld_mypy.bat
    Co-authored-by: James Robertson <jamierobertsongames@gmail.com>

--- recipe/bld_mypy.bat
+++ recipe/bld_mypy.bat
@@ -1,7 +1,7 @@
 @echo on
 
-IF "%PY_VER%"!="3.13" (
-REM TODO re-enable on 1.12
-set MYPY_USE_MYPYC=1
+IF NOT "%PY_VER%"=="3.13" (
+    REM TODO re-enable on 1.12
+    set MYPY_USE_MYPYC=1
 )
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
