db92758c427f2fb8ef3c2a1b044b61e034f234db
Brian Wing <bwing@anaconda.com>
2025-01-13 08:50:54 -0500

    Updated to 1.14.1

--- recipe/bld_mypy.bat
+++ recipe/bld_mypy.bat
@@ -1,7 +1,4 @@
 @echo on
 
-IF NOT "%PY_VER%"=="3.13" (
-    REM TODO re-enable on 1.12
-    set MYPY_USE_MYPYC=1
-)
+set MYPY_USE_MYPYC=1
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
